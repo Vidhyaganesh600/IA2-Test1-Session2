8320a4dd4e0d17f66f87e7b5e4638af6d144fee8
Vidhyaganesh600 <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-02-13 09:55:16 +0000

    p1debuglog.txt

---
+++ p8final.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+struct _triangle
+{
+  float base,altitude,area;
+};
+typedef struct _triangle Triangle
+int input_n()
+{
+int n;
+printf("enter the members of triangle");
+scanf("%d",&n);
+return n;
+}
+Triangle input_triangle()
+{
+  Triangle Triangle1;
+  printf("enter the members of Triangle1");
+  scanf("%f%f",7Trian)
+}
